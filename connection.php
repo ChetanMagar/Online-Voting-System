6c771c9139d2bfb54e10aec04cb0ca40ec48c32e
ChetanMagar <61990759+ChetanMagar@users.noreply.github.com>
2020-08-02 00:27:26 -0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,7 @@
+<?php 
+	$server="sql306.epizy.com"
+	$username="epiz_26411832"
+	$password="wrz1Ar4eY3JxN"
+	$dbname="epiz_26411832_onlinevoting"
+$con = mysqli_connect($server,$username,$password,$dbname) or die ("error" . mysqli_error($con));
+?>
---
+++ footer.php
@@ -0,0 +1,7 @@
+<br>
+<br>
+<br>
+<br>
+<center><font color="red"><b><i>Project:</i></b></font><i>XAMPP</i></center>
+</body>
+</html>
